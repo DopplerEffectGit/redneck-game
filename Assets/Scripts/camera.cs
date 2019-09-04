e72af0495e241889755bf356c413f8b88c8e6668
Grigorill <54493388+Grigorill@users.noreply.github.com>
2019-09-04 15:42:56 +0300

    Новый уровень и камера
    уровень NEW NEW Grigorill хз че с анимацией надо бы может новую накинуть хзхззхз

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -7,6 +7,8 @@ public class camera : MonoBehaviour
     // Start is called before the first frame update
     private Transform Player;
     public float damping;
+    public float xp;
+    public float yp;
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -15,7 +17,8 @@ public class camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 v2 = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+        
+        Vector3 v2 = new Vector3(Player.position.x+xp, Player.position.y+yp, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, v2, damping * Time.deltaTime);
     }
 }
