028f751cff259761de4c5f5d6440e87412c04eac
DopplerEffectGit <planemo.q@gmail.com>
2019-09-07 21:07:45 +0300

    коллизии, безполезные кнопки х2
    добавлена реакция на столкновение гг с землей, платформой и ящиками.
    добавлена анимация прыжка + зависимости.
    
    добавлыны кнопки.
    ни одна,  падлаЮ не работает. скорее всего дело в слорях и просто не проходят онклики.

--- Assets/Scripts/data/model/hero.cs
+++ Assets/Scripts/data/model/hero.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hero : MonoBehaviour
 {
@@ -9,29 +10,50 @@ public class hero : MonoBehaviour
     public float jumpSpeed = 8f;
     private float movement = 0f;
     private Rigidbody2D rigidBody;
-    public Transform groundCheckPoint;
-    public float groundCheckRadius;
-    public LayerMask groundLayer;
-    private bool isTouchingGround;
     private Animator playerAnimation;
+    private bool onGround;
 
-    public BoxCollider2D groundCollider;
+    public Button buttonLeft;
+    public Button buttonRight;
+    public Button buttonJump;
+
     // Use this for initialization
     void Start()
     {
-        groundCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
 
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
         playerAnimation = GetComponent<Animator>();
+
+
+
+
+
+        buttonLeft.onClick.AddListener(delegate () {
+
+            Debug.Log("left button ");
+
+        });
+
+
+
+        //buttonLeft = (Instantiate(button) as Button).gameObject;
+
+        //buttonLeft = GameObject.FindGameObjectWithTag("buttonLeft");
+        //buttonLeft = gameObject.GetComponent<Text>().text;
+
+
+        //buttonLeft = GameObject.FindGameObjectWithTag("buttonLeft") as Button;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("hero update");
+        //Debug.Log("hero update");
 
-//        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
         movement = Input.GetAxis("Horizontal");
         if (movement > 0f)
         {
@@ -48,22 +70,76 @@ public class hero : MonoBehaviour
         {
             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && onGround)
         {
             Debug.Log("hero jump");
 
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
         }
         playerAnimation.SetFloat("speed", Mathf.Abs(rigidBody.velocity.x));
-        //playerAnimation.SetBool("onGround", isTouchingGround);
+        playerAnimation.SetBool("onGround", onGround);
+
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log("OnTriggerEnter2D: " + other.name);
+        if (
+            col.transform.name.Contains("road") ||
+            col.transform.name.Contains("box") ||
+            col.transform.name.Contains("platform")) 
+        {
+            Debug.Log("OnCollisionEnter2D " + col.transform.name);
 
-        
+            onGround = true;
+
+        }
+
     }
 
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (
+                    col.transform.name.Contains("road") ||
+                    col.transform.name.Contains("box") ||
+                    col.transform.name.Contains("platform"))
+        {
+            Debug.Log("OnCollisionExit2D " + col.transform.name);
+            onGround = false; 
+
+        }
+    }
+
+    public void leftButtonClick() {
+        Debug.Log("left button ");
+
+
+    }
+
+    public void rightButtonClick()
+    {
+        Debug.Log("right button ");
+
+
+    }
+    public void jumpButtonClick()
+    {
+        Debug.Log("jump button ");
+
+
+    }
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    Debug.Log("OnTriggerEnter2D: " + other.name);
+
+
+    //}
+
+
+    //void OnTriggerExit2D(Collider2D collider)
+    //{
+    //    //Debug.Log("OnTriggerExit2D Here!");
+    //    print("No longer in contact with " + collider.transform.name);
+
+    //}
 
 }
