6218b6e64a6fa14a48495ae4a5d7e63cd921ff53
Grigorill <54493388+Grigorill@users.noreply.github.com>
2019-09-08 11:23:30 +0300

    controller

---
+++ Assets/PixelCrown/Character/Demo_Scenes/Destroy1Sec.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy1Sec : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 1.0f);
+    }
+
+}
