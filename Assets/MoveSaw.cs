5f02aad51cb89479a3390cd260b4a7e808eb28e6
Grigorill <54493388+Grigorill@users.noreply.github.com>
2019-09-17 21:06:03 +0300

    saw

---
+++ Assets/MoveSaw.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSaw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 5);
+        transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
+
+    }
+
+    private void OnBecameInvisible()
+    {
+      
+        Destroy(gameObject);
+    }
+
+    
+}
---
+++ Assets/SpawnSaw.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSaw : MonoBehaviour
+{
+    [SerializeField] GameObject Saw;
+    [SerializeField] Transform SpawnPoint;
+    [SerializeField] float SpawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(SpawnTime);
+            Instantiate(Saw, SpawnPoint);
+        }
+    }
+}
